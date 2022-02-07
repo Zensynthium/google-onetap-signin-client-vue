fc9f516c2f53a20e874a7825d300ead03d9687cc
Zensynthium <47935832+Zensynthium@users.noreply.github.com>
2022-02-07 15:33:44 -0500

    Update googleOneTapSignin.js

--- src/composables/googleOneTapSignin.js
+++ src/composables/googleOneTapSignin.js
@@ -2,7 +2,7 @@ import googleOneTap from 'google-one-tap';
 
 export default function() {
   const axios = require('axios');
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
 
   const clientID = process.env.VUE_APP_CLIENT_ID
 
@@ -17,9 +17,16 @@ export default function() {
     googleOneTap(options, (res) => {
 	  	// Send response to server
 	  	console.log(res);
-    
+      
+      // This should dynamically grab the current url of the page, though the currentUrl must also be whitelisted on the server side's cors configuration or else it may throw errors
+      const currentUrl = window.location.href
+
+      const options = {
+        headers: { "Access-Control-Allow-Origin": currentUrl }
+      }
+
       // Google One-Tap Signin sends a POST request which must be sent to a server to be processed.
-      axios.post(`${process.env.VUE_APP_API_URL}/verify-token`, res)
+      axios.post(`${process.env.VUE_APP_API_URL}/verify-token`, res, options)
         .then(res => {
           console.log(res);
         })
